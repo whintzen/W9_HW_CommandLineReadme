3c39c76db1a016980da8ad12d323ade278a0c725
Wendy Hintzen <wpah@comcast.net>
2020-11-16 01:32:01 -0500

    Updated all the files

---
+++ ExampleREADME.md
@@ -0,0 +1,56 @@
+# README.md Generator: Node.js and ES6+
+  
+
+![GitHub license](https://img.shields.io/badge/license-MIT-blue.svg)
+
+
+## Description 
+
+It is important to have a quality README with information about the app--what is the app for, how to use the app, 
+
+## Table of Contents 
+
+* [Installation](#installation)
+
+* [Usage](#usage)
+
+* [License](#license)
+
+* [Contributing](#contributing)
+
+* [Tests](#tests)
+
+* [Questions](#questions)
+
+## Installation
+
+To install necessary dependencies, run the following command:
+
+```
+Steps required to install project and how to get the development environment running:
+```
+
+## Usage
+
+go to my github
+
+## License
+
+This project is licensed under the MIT license.
+  
+## Contributing
+
+go to my github
+
+## Tests
+
+To run tests, run the following command:
+
+```
+The application itself can be invoked with node index.js.
+```
+
+## Questions
+
+If you have any questions about the repo, open an issue or contact me directly at wpah@comcast.net. You can find more of my work at [whintzen](https://github.com/whintzen/).
+
---
+++ index.js
@@ -0,0 +1,80 @@
+const fs = require("fs");
+const path = require("path");
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
+
+// array of questions for user
+const questions = [
+  {
+    type: "input",
+    name: "github",
+    message: "What is your GitHub username?"
+  },{
+    type: "input",
+    name: "email",
+    message: "What is your email address?"
+  },
+  {
+    type: "input",
+    name: "repo",
+    message: "What is the name of your Github repo?",
+    default: "CommandLineReadme"
+  },
+  {
+    type: "input",
+    name: "title",
+    message: "What is your project's name?",
+    default: "Project Title"
+  },
+  {
+    type: "input",
+    name: "description",
+    message: "Please write a short description of your project",
+    defaut: "Project Description"
+  },
+  {
+    type: "list",
+    name: "license",
+    message: "What kind of license should your project have?",
+    choices: ["MIT", "APACHE 2.0", "GPL 3.0", "BSD 3", "None"]
+  },
+  {
+    type: "input",
+    name: "installation",
+    message: "What command should be run to install dependencies?",
+    default: "npm i"
+  },
+  {
+    type: "input",
+    name: "test",
+    message: "What command should be run to run tests?",
+    default: "npm test"
+  },
+  {
+    type: "input",
+    name: "usage",
+    message: "What does the user need to know about using the repo?",
+  },
+  {
+    type: "input",
+    name: "contributing",
+    message: "What does the user need to know about contributing to the repo?",
+  }
+];
+
+//Function to write README file
+function writeToFile(fileName, data) {
+  return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+}
+
+//function to initialize the program
+function init() {
+  inquirer.prompt(questions)
+  .then((inquirerResponses) => {
+    console.log("Generating README...");
+    writeToFile("README.md", generateMarkdown({...inquirerResponses}));
+  })
+}
+
+// function call to initialize the program
+init();
